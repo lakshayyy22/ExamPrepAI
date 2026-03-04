736c4a0989fe7f599ee264abe751bab27d46fea5
Lakshay <lakshaysaini2676@gmail.com>
2026-03-04 12:00:20 +0530

    remove windows ocr

--- ocr_pdf.py
+++ ocr_pdf.py
@@ -2,15 +2,10 @@ import pytesseract
 from pdf2image import convert_from_path
 import sys
 import json 
-# Tesseract path
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
-# POPPLER PATH (THIS FIXES YOUR ERROR)
-#poppler_path = r"C:\poppler\poppler-25.12.0\Library\bin"
 
 pdf_path = sys.argv[1]
 
-pages = convert_from_path(pdf_path)
+pages = convert_from_path(pdf_path, dpi=300)
 
 full_text = ""
 
